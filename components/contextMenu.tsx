16794417d1d15747cfcdd0cf21958b8323d15d07
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2026-01-18 23:54:09 +0100

    customName to add user

--- components/contextMenu.tsx
+++ components/contextMenu.tsx
@@ -15,7 +15,7 @@ function createNotificationMenuItem(channelId: string, name: string) {
     const bypass = isBypass(channelId);
 
     const handleAdd = () => {
-        const user: User = { id: "usuario-" + bypassLen(), channel: channelId, name: name, activated: true };
+        const user: User = { id: "usuario-" + bypassLen(), channel: channelId, customName: name, name: name, activated: true };
         addUser(user);
     };
 
